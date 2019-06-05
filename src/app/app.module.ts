d8c174def13c174612ca61116dfc365e41ba7049
xufang775 <xufang461010923@163.com>
2019-06-05 13:32:14 +0800

    还原到空项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {PeekABooComponent} from './peek-a-boo.component';
-import {PeekABooParentComponent} from './peek-a-boo-parent.component';
-import {InputFocusComponent} from './input-focus.component';
 import {FormsModule} from '@angular/forms';
-import {OnChangesComponent, OnChangesParentComponent} from './on-changes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeekABooComponent,
-    PeekABooParentComponent,
-    InputFocusComponent,
-    OnChangesComponent,OnChangesParentComponent,
   ],
   imports: [
     BrowserModule,
